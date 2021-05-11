5f1b0f7c320aed31735d31f8ca849171db1143b1
Sajeed <shaiksajeed16@gmail.com>
2021-05-11 22:34:53 +0530

    Practice script to manipulate DOM

---
+++ index.js
@@ -0,0 +1,6 @@
+const CE1=document.createElement('div');
+const CE1I= CE1.innerHTML=" I am a developer";
+const M1DISP=document.getElementById("m1Display");
+M1DISP.appendChild(CE1);
+console.log(M1DISP);
+console.log("Hello");
