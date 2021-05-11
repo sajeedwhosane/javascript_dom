16f781195f2a164525a723b9187c314bcb1ae3ce
Sajeed <shaiksajeed16@gmail.com>
2021-05-11 22:55:00 +0530

    Manipulating DOM using method 1

---
+++ README.md
@@ -0,0 +1,1 @@
+## practice script to manipulate DOM using javascript.
--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const CE1=document.createElement('div');
-const CE1I= CE1.innerHTML=" I am a developer";
-const M1DISP=document.getElementById("m1Display");
-M1DISP.appendChild(CE1);
-console.log(M1DISP);
-console.log("Hello");
+/*
+Practice script to learn how to create an element, edit the contents in the element and then append
+it to another element
+*/
+
+const M1B=document.getElementById("m1");
+M1B.addEventListener('click',function(){
+    const CE1=document.createElement('div');
+    const CE1I= CE1.innerHTML=`<code>
+        const M1B=document.getElementById("m1"); <br>
+        M1B.addEventListener(click,function(){ <br>
+        const CE1=document.createElement('div'); <br>
+        const CE1I= CE1.innerHTML="Exaample"; <br>
+        const M1DISP=document.getElementById("m1Display"); <br>
+        M1DISP.appendChild(CE1); <br>
+        </code>
+    });`;
+    const M1DISP=document.getElementById("m1Display");
+    M1DISP.appendChild(CE1);
+});
